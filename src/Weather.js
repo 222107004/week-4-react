596264eb63e855fcd1b0298f26999f08090fb4af
222107004 <thama.mabatha@gmail.com>
2024-07-29 17:58:54 +0200

    Chabing codesandox code to react code

--- src/Weather.js
+++ src/Weather.js
@@ -1,8 +1,69 @@
-import React from "react";
+import React,{useState} from "react";
 import axios from "axios";
 
-export default function Weather() {
-  let apiKey =
-  
-    return <h2>Hello from Weather</h2>;
+export default function SearchEngine() {
+  const [city, setCity] = useState("");
+  const [loaded, setLoaded] = useState(false);
+  const [weather, setWeather] = useState({
+    temperature: null,
+    wind: null,
+    humidity: null,
+    icon: null,
+    description: null,
+  });
+
+  function displayWeather(response) {
+    setWeather({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description,
+    });
+    setLoaded(true);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const apiKey = "215576bab28022db35e6e64f040e1b56";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    axios.get(url).then(displayWeather);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  if (loaded) {
+    return (
+      <div>
+        <h2>
+          <li>
+            Temperature in {city}: {Math.round(weather.temperature)}°C
+          </li>
+          <li>Humidity: {weather.humidity}%</li>
+          <li>Wind: {weather.wind} km/h</li>
+          <li>
+            <img src={weather.icon} alt={weather.description} />
+          </li>
+          <li>Description: {weather.description}</li>
+        </h2>
+      </div>
+    );
+  } else {
+    return (
+      <div className="WeatherSearch">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="search"
+            placeholder="Enter a city..."
+            onChange={updateCity}
+          />
+          <input type="submit" value="Search" />
+        </form>
+      </div>
+    );
+  }
 }
