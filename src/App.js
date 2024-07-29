657917355061964a402a396c511a8a55fab8b623
222107004 <thama.mabatha@gmail.com>
2024-07-29 19:43:48 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
-import logo from "./logo.svg";
+
 import "./App.css";
 import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Weather />
-      </header>
+      <Weather />
     </div>
-   
-   
   );
-  
-  
 }
 
 export default App;
